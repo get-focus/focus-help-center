bddf8a163b3b33ce821dca4429b8b959b341f26e
GuenoleK <kikabouguenole@gmail.com>
2016-06-01 18:24:04 +0200

    Updating webpack configuration and server configuration

---
+++ app/back-office/index.js
@@ -0,0 +1,3 @@
+import React , { Component , PropTypes } from 'react';
+
+React.render(<h1>Coucou</h1>, document.body);
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,21 +1,31 @@
 const path  = require('path');
-module.exports =  {
-    entry: {
-        back: './back-office/index',
-        extension: './extension/index'
-    },
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = (app, isDev) => ({
+    entry: Object.assign({
+        [app]: `./${app}/index`
+    }, isDev ? [`webpack-dev-server/client?http://localhost:9999`, 'webpack/hot/only-dev-server'] : {}),
     output: {
         path: path.join(__dirname, 'dist'),
         filename: '[name].js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './common/index.html',
+            filename: `${app}.html`
+        })
+    ].concat(isDev ? [
+        new webpack.HotModuleReplacementPlugin()
+    ] : []),
     loaders: [
         {
             test: /\.jsx?$/,
-            include: /(node_modules|bower_components)/,
-            loader: 'babel',
+            include: ['common', app],
+            loader: isDev ? 'react-hot!babel' : 'babel',
             query: {
                 presets: ['focus']
             }
         }
     ]
-};
+});
