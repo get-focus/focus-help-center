6ba76babfda0ac90b882e1ee946c8f43aa18fbbe
GuenoleK <kikabouguenole@gmail.com>
2016-06-02 18:44:50 +0200

    Initializing files and updating package.json for the tests commands

--- app/test/mocha-init.js
+++ app/test/mocha-init.js
@@ -11,17 +11,15 @@ const React = require('react');
 // Globals
 global.React = require('react');
 global.ReactDOM = require('react-dom');
-global.chai = require('chai');
+global.chai = chai;
 global.sinon = sinon;
 global.TestUtils = require('react-addons-test-utils');
 global.sandbox = require('./sandbox');
 global.componentHandler = {upgradeElement: function(){}};
-
 // Js dom
 import jsdom from 'jsdom';
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
-
 // take all properties of the window object and also attach it to the
 // from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
 function propagateToGlobal (window) {
@@ -31,9 +29,9 @@ function propagateToGlobal (window) {
         global[key] = window[key];
     }
 }
-
 // mocha global object
 propagateToGlobal(window);
+
 
 process.on('unhandledRejection', (error)=>{
     console.error('Unhandled Promise Rejection:');
