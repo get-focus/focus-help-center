49362149e068843e439b74bcacf729e4e032de41
GuenoleK <kikabouguenole@gmail.com>
2016-06-02 11:35:02 +0200

    Minor changes

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 
 module.exports = (app, isDev) => ({
     entry: Object.assign({
-        [app]: `./${app}/index.jsx`
+        [app]: `./${app}/index`
     }, isDev ? [`webpack-dev-server/client?http://localhost:9999`, 'webpack/hot/only-dev-server'] : {}),
     output: {
         path: path.resolve('dist'),
