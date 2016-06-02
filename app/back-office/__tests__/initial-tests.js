fd3c78249becf2ada570f454f1893f852f68819f
GuenoleK <kikabouguenole@gmail.com>
2016-06-02 18:45:11 +0200

    Adding basic tests

---
+++ app/back-office/__tests__/initial-tests.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+const {renderIntoDocument, Simulate} = TestUtils;
+
+const VeryFirstDiv = React.createClass ({
+    render() {
+        const {click} = this.props;
+        return (
+            <div>
+                <span ref='myTest' id='title' onClick={click}>Lovely! Here it is - my very first React component!</span>
+            </div>
+        );
+    }
+});
+
+describe('Basic tests', () => {
+    describe('React and ReactDom', () => {
+        let testComponent;
+        before(() => {
+            testComponent = renderIntoDocument(<VeryFirstDiv />);
+        });
+        it('Should have an id named `title`', () => {
+            chai.expect(testComponent.refs.myTest.id).to.equal('title');
+        });
+    });
+    describe('Sinon Spy', () => {
+        let testComponent, onClickSpy;
+        before(() => {
+            onClickSpy = sinon.spy();
+            function onClickSpan() {
+                console.log('Success');
+                onClickSpy();
+            }
+            testComponent = renderIntoDocument(<VeryFirstDiv click={onClickSpan}/>);
+            Simulate.click(testComponent.refs.myTest)
+        });
+        it('Should be called at least once', () => {
+            chai.expect(onClickSpy).to.have.been.called.once;
+        });
+    });
+});
