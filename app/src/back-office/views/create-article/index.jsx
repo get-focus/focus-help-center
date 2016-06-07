db9cc5417bef1b348dfa29ca7722cd12a3eca284
GuenoleK <kikabouguenole@gmail.com>
2016-06-07 16:11:20 +0200

    Creating the edit page

--- app/src/back-office/views/create-article/index.jsx
+++ app/src/back-office/views/create-article/index.jsx
@@ -1,12 +1,12 @@
-//Style
-
 // Components
+import {EditPage} from '../../../common/components/edition-page';
 
 /** Root component of the back-office app. */
 export function CreateArticleView() {
     return (
         <div>
-            <h3>Create Article Page</h3>
+            <h3>Creation Page</h3>
+            <EditPage />
         </div>
     );
 }
