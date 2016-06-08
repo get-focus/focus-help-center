3f0439133d28ca3fb7708295ba2747d0baee69fb
GuenoleK <kikabouguenole@gmail.com>
2016-06-08 13:31:17 +0200

    Finalizing the template

--- app/src/common/components/article-edition/index.jsx
+++ app/src/common/components/article-edition/index.jsx
@@ -2,13 +2,26 @@
 import {Component} from 'react';
 import {Link} from 'react-router';
 import {ContentArea} from './content-area';
+import ReactDOM from 'react-dom';
 
 export class EditPage extends Component {
+
+    state = {
+        isVisible: true
+    };
+
     componentDidMount() {
         componentHandler.upgradeDom();
     }
 
+    parameterButtonHandler() {
+        const {isVisible} = this.state;
+        this.setState({isVisible: isVisible ? false : true});
+    }
+
     render() {
+        const {isVisible} = this.state;
+        let classVisibility = isVisible ? '' : '-hidden';
         return (
             <div>
                 <div className='edit-page-container'>
@@ -18,33 +31,50 @@ export class EditPage extends Component {
                 </div>
 
                 <div className='edit-page-container'>
-                    <div className='edit-parameters-item'>
-                        <h5>PARAMÉTRAGE
-                            <button className="mdl-button mdl-js-button mdl-button--icon edit-button">
-                                <i className="material-icons">settings</i>
-                            </button>
-                        </h5><br/>
+                    <div className={`edit-parameters-item${classVisibility}`} ref='parametersBloc'>
+                        <div className='edit-parameters-bloc'>
+                            <h5>PARAMÉTRAGE</h5><br/>
+                            <span className='edit-parameters-label'>RUBRIQUES</span>
+                            <div className='mdl-button mdl-js-button mdl-js-ripple-effect edit-button'>
+                                editer
+                            </div><br/>
 
-                        <span className='edit-span-parameters'>Titre</span>
-                        <div className='mdl-button mdl-js-button mdl-js-ripple-effect edit-button'>
-                            editer
-                        </div><br/>
+                            <div className="mdl-textfield mdl-js-textfield">
+                                <input className='mdl-textfield__input' type='text' id='sample1' />
+                                <label className="mdl-textfield__label" htmlFor="sample1">Rubriques...</label>
+                            </div>
 
-                        <div className="mdl-textfield mdl-js-textfield">
-                            <input className='mdl-textfield__input' type='text' id='sample1' />
-                            <label className="mdl-textfield__label" htmlFor="sample1">Titre...</label>
+                            <br/><br/>
+                            <span className='edit-parameters-label'>URL CONTEXTUELLE</span>
+                            <div className='mdl-button mdl-js-button mdl-js-ripple-effect edit-button'>
+                                editer
+                            </div><br/>
+
+                            <div className="mdl-textfield mdl-js-textfield">
+                                <input className='mdl-textfield__input' type='text' id='sample1' />
+                                <label className="mdl-textfield__label" htmlFor="sample1">URL...</label>
+                            </div>
+
+                            <br/><br/>
+                            <span className='edit-parameters-label'>BLOC D'INFORMATION CONTEXTUEL</span>
+                            <div className='mdl-button mdl-js-button mdl-js-ripple-effect edit-button'>
+                                editer
+                            </div><br/>
+
+                            <div className="mdl-textfield mdl-js-textfield">
+                                <input className='mdl-textfield__input' type='text' id='sample1' />
+                                <label className="mdl-textfield__label" htmlFor="sample1">Bloc d'information...</label>
+                            </div>
                         </div>
+                    </div>
 
-                        <br/><br/>
-                        <span className='edit-span-parameters'>Description</span>
-                        <div className='mdl-button mdl-js-button mdl-js-ripple-effect edit-button'>
-                            editer
-                        </div><br/>
-
-                        <div className="mdl-textfield mdl-js-textfield">
-                            <input className='mdl-textfield__input' type='text' id='sample1' />
-                            <label className="mdl-textfield__label" htmlFor="sample1">Description...</label>
-                        </div>
+                    <div className='edit-parameters-button-zone'>
+                        <button
+                        className="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect edit-button parameters-icon"
+                        onClick={this.parameterButtonHandler.bind(this)}
+                        >
+                            <i className="material-icons">settings</i>
+                        </button>
                     </div>
 
                     <div className='edit-zone-item'>
