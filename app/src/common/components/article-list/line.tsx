2737410c17557a7752c0a6f6e7c9ab298e42ff63
Damien FRIKHA <dfrikha@KLEE.LAN.NET>
2016-06-10 14:12:44 +0200

    Hide edit/create buttons when not connected.

--- app/src/common/components/article-list/line.tsx
+++ app/src/common/components/article-list/line.tsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router';
 import i18n from 'i18next';
 
 /** Component that displays an article as a line. */
-export function ArticleLine({article: {id, title, description}}: {article: Article}) {
+export function ArticleLine({article: {id, title, description}, canEdit}: {article: Article, canEdit: boolean}) {
     return (
         <div className='article-list-item'>
             <i className='material-icons'>receipt</i>
@@ -15,10 +15,12 @@ export function ArticleLine({article: {id, title, description}}: {article: Artic
                     {description}
                 </div>
             </Link>
-            <Link className='mdl-button mdl-js-button mdl-button--raised mdl-button--colored article-list-item-button' to={`edit-article/${id}`}>
-                <i className='material-icons'>edit</i>
-                {i18n.t('article-list.item.edit')}
-            </Link>
+            {canEdit ?
+                <Link className='mdl-button mdl-js-button mdl-button--raised mdl-button--colored article-list-item-button' to={`edit-article/${id}`}>
+                    <i className='material-icons'>edit</i>
+                    {i18n.t('article-list.item.edit')}
+                </Link>
+            : ''}
         </div>
     );
 }
