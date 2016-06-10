fc778c544f888475c1aa9e00b906f7345e030d3d
GuenoleK <kikabouguenole@gmail.com>
2016-06-10 10:17:09 +0200

    Adding loadArticle

--- app/src/common/actions/article-detail.ts
+++ app/src/common/actions/article-detail.ts
@@ -5,10 +5,28 @@ import {Api} from '../server/index';
 
 /** Action creator to update article in store. */
 
+export function loadArticleDetail(article: Article): any {
+    console.log(article);
+    return async (dispatch, getState) => {
+        try {
+            dispatch(successLoadArtictleDetail(article));
+        } catch (err){
+            throw err;
+        }
+    };
+}
+
+function successLoadArtictleDetail(article: Article): ArticleDetailAction {
+    return {
+        type: Action.LOAD_ARTICLE,
+        article
+    };
+}
+
 /**Update article action */
 export function updateArticle(article: Article, attribute?: string, value?: string): any {
     article[`${attribute}`] = value;
-    return async (dispatch) => {
+    return async (dispatch, getState) => {
         try {
             dispatch(successUpdateArtictleDetail(article));
         } catch (err) {
@@ -17,7 +35,7 @@ export function updateArticle(article: Article, attribute?: string, value?: stri
     };
 }
 
-export function successUpdateArtictleDetail(article: Article): ArticleDetailAction {
+function successUpdateArtictleDetail(article: Article): ArticleDetailAction {
     return {
         type: Action.UPDATE_ARTICLE,
         article
@@ -35,7 +53,7 @@ export function saveArticle(article: Article): any {
     };
 }
 
-export function successSaveDetail(success: boolean, article: Article): ArticleDetailAction {
+function successSaveDetail(success: boolean, article: Article): ArticleDetailAction {
     return {
         type: Action.SUCCESS_SAVE_ARTICLE,
         success,
@@ -43,7 +61,7 @@ export function successSaveDetail(success: boolean, article: Article): ArticleDe
     };
 }
 
-export function failSaveDetail(error: string): ArticleDetailAction {
+function failSaveDetail(error: string): ArticleDetailAction {
     return {
         type: Action.FAILURE_SAVE_ARTICLE,
         error
--- app/src/common/reducers/article-detail.ts
+++ app/src/common/reducers/article-detail.ts
@@ -1,14 +1,19 @@
 import {Action} from '../actions';
 import {ArticleDetailAction, ArticleDetailState} from '../definitions/article-detail';
 
-export const defaultValue: ArticleDetailState = {isLoading: false};
+export const defaultValue: ArticleDetailState = { isLoading: false };
 
 /** Reducer that handles the `articleDetail` store node. */
 export function articleDetail(state: ArticleDetailState = defaultValue, action: ArticleDetailAction) {
     switch (action.type) {
+        case Action.LOAD_ARTICLE:
+            return {
+                article: action.article
+            };
         case Action.UPDATE_ARTICLE:
-            // TODO: update state with only non-undefined article members.
-            return state;
+            return {
+                article: action.article
+            };
         default:
             return state;
     }
