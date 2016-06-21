466d46716b80f12f7b7145269b0ee1f45741304f
Damien FRIKHA <dfrikha@KLEE.LAN.NET>
2016-06-21 14:11:50 +0200

    Update dependencies

--- app/src/common/store/index.ts
+++ app/src/common/store/index.ts
@@ -1,10 +1,10 @@
 import {createStore, applyMiddleware} from 'redux';
-import * as thunk from 'redux-thunk';
+import thunk from 'redux-thunk';
 import {rootReducer} from '../reducers';
 import {api} from '../server/api';
 import {defaultState} from './default-state';
 
 /** Creates the application store. */
 export function configureStore() {
-    return createStore(rootReducer, defaultState, applyMiddleware(thunk.default['withExtraArgument'](api)));
+    return createStore(rootReducer, defaultState, applyMiddleware(thunk['withExtraArgument'](api)));
 }
