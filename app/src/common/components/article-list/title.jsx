9df09ca06588fa52e1ce64b48ecf75ac65f9cf86
GuenoleK <kikabouguenole@gmail.com>
2016-06-24 14:01:51 +0200

    Updating article list to have default props

--- app/src/common/components/article-list/title.jsx
+++ app/src/common/components/article-list/title.jsx
@@ -5,15 +5,7 @@ import {searchArticleList} from '../../actions/article-list';
 import i18n from 'i18next';
 import {TextField, LinearProgress} from 'material-ui';
 
-export default connect(
-    state => ({
-        filter: state.articleList.filter,
-        loading: state.articleList.isLoading,
-        error: state.articleList.error
-    }),
-    dispatch => ({search: filter => dispatch(searchArticleList(filter))})
-)(function ArticleListTitle({loading, filter, search, error, textFieldUnderline}) {
-    const showUnderline = textFieldUnderline || true;
+const ArticleListTitle = ({loading, filter, search, error, textFieldUnderline}) => {
     return (
         <div className='article-list-header'>
             <h3>{i18n.t('article-list.title')}</h3>
@@ -21,16 +13,29 @@ export default connect(
                 <i className='material-icons'>search</i>
                 <div className='search-bar'>
                     <TextField
-                        underlineShow={showUnderline && !(loading && filter)}
+                        underlineShow={textFieldUnderline && !(loading && filter)}
                         errorText={error ? ' ' : null}
                         errorStyle={{color: 'indianred'}}
                         value={filter}
                         onChange={e => search(e.target.value)}
                         hintText={i18n.t('search.placeholder')}
                     />
-                    {showUnderline && loading && filter ? <LinearProgress style={{position: 'absolute', bottom: '8px', height: '2px'}} /> : null}
+                    {textFieldUnderline && loading && filter ? <LinearProgress style={{position: 'absolute', bottom: '8px', height: '2px'}} /> : null}
                 </div>
             </div>
         </div>
     );
-});
+};
+
+ArticleListTitle.defaultProps = {
+    textFieldUnderline: true
+};
+
+export default connect(
+    state => ({
+        filter: state.articleList.filter,
+        loading: state.articleList.isLoading,
+        error: state.articleList.error
+    }),
+    dispatch => ({search: filter => dispatch(searchArticleList(filter))})
+)(ArticleListTitle);
