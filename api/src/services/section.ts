988a37c158756933392546473ce0c38aafc24127
GuenoleK <kikabouguenole@gmail.com>
2016-06-28 11:37:11 +0200

    Adding the fin section, save section and delete section

--- api/src/services/section.ts
+++ api/src/services/section.ts
@@ -74,9 +74,132 @@ export function sectionService(app: express.Application) {
         const section = await Section.findById(req.params.id);
         if (!section) {
             res.status(404);
-            res.json({error: 'This section doesn\'t exists'});
+            res.json({ error: 'This section doesn\'t exists' });
         } else {
             res.json(section);
+        }
+    });
+
+    /**
+     * @swagger
+     * /api/section:
+     *   get:
+     *     tags:
+     *       - Section
+     *     description: Returns all the sections.
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: filter
+     *         description: Optional filter by name.
+     *         in: query
+     *         required: false
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Returns all the sections.
+     *         schema:
+     *         type: array
+     *         items:
+     *             $ref: '#/definitions/Section-Get'
+     */
+    app.get(/\/api\/section(\?filter=:filter)?/, async (req, res) => {
+        let {filter} = req.query;
+        const order = [fn('lower', col('name'))];
+        if (filter) {
+            const like = or({ name: { like: `%${filter}%` } });
+            if (req.user && req.user.signedIn) {
+                res.json(await Section.findAll({ where: [like], order }));
+            }
+        } else {
+            console.log('Right here');
+            if (req.user && req.user.signedIn) {
+                console.log('Right there');
+                res.json(await Section.findAll({ order }));
+            }
+        }
+    });
+
+    /**
+     * @swagger
+     * /api/section:
+     *   post:
+     *     tags:
+     *       - Section
+     *     description: Saves a section.
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: section
+     *         description: The sectop, to save.
+     *         in: body
+     *         required: true
+     *         schema:
+     *           $ref: '#/definitions/Section-Post'
+     *     responses:
+     *       200:
+     *         description: The saved section with its id.
+     *         schema:
+     *           $ref: '#/definitions/Section-Get'
+     *       403:
+     *         description: No rights to save
+     *         schema:
+     *           $ref: '#/definitions/Error'
+     */
+    app.post('/api/section', async (req, res) => {
+        if (!(req.user && req.user.signedIn)) {
+            res.status(403);
+            res.json({ error: 'Cannot save a section when not connected' });
+        } else {
+            let section: ISection = req.body;
+            if (!section.id) {
+                section = (await Section.create(req.body)).get();
+            }
+            res.json(section);
+        }
+    });
+
+    /**
+     * @swagger
+     * /api/section/{id}:
+     *   delete:
+     *     tags:
+     *       - Section
+     *     description: Deletes a section.
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: id
+     *         description: Id of the section to delete.
+     *         in: path
+     *         required: true
+     *         type: integer
+     *     responses:
+     *       200:
+     *         description: Success message
+     *         schema:
+     *           $ref: '#/definitions/Success'
+     *       403:
+     *         description: No rights to delete
+     *         schema:
+     *           $ref: '#/definitions/Error'
+     *       404:
+     *         description: Can't find the section
+     *         schema:
+     *           $ref: '#/definitions/Error'
+     */
+    app.delete('/api/section/:id', async (req, res) => {
+        if (!(req.user && req.user.signedIn)) {
+            res.status(403);
+            res.json({ error: 'Cannot delete a section when not connected' });
+        } else {
+            const deletedRows = await Section.destroy({ where: { id: req.params.id } });
+            if (deletedRows === 1) {
+                res.json({ success: true });
+            } else {
+                res.status(404);
+                res.json({ error: `No section deleted` });
+            }
         }
     });
 }
