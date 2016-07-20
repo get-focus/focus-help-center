ac324e04726edab072d59f8a9b9944895ee94ecd
GuenoleK <kikabouguenole@gmail.com>
2016-07-20 16:50:09 +0200

    Another minor change

--- api/src/services/section.ts
+++ api/src/services/section.ts
@@ -7,17 +7,23 @@ export function sectionService(prefix: string, app: express.Application) {
     app.get(`${prefix}/api/section/:id`, async (req, res) => {
         const section = (await Section.findById(req.params.id));
 
-        if (!(req.user && req.user.signedIn)) {
-            res.status(403);
-            res.json({ error: 'Cannot search a section when not connected' });
-        } else {
-            if (!section) {
-                res.status(404);
-                res.json({ error: 'No section found' });
+        try {
+            if (!(req.user && req.user.signedIn)) {
+                res.status(403);
+                res.json({ error: 'Cannot search a section when not connected' });
             } else {
-                res.json(section.get());
+                if (!section) {
+                    res.status(404);
+                    res.json({ error: 'No section found' });
+                } else {
+                    res.json(section.get());
+                }
             }
+        } catch (e) {
+            res.status(403);
+            res.json({error: e});
         }
+
     });
 
     app.get(`${prefix}/api/section/:id/articles`, async (req, res) => {
@@ -35,28 +41,33 @@ export function sectionService(prefix: string, app: express.Application) {
 
     app.get(`${prefix}/api/section`, async (req, res) => {
         const order = [fn('lower', col('name'))];
-        if (req.user && req.user.signedIn) {
-            const sections = ((await Section.findAll({order})).map(section => section.get()));
-            res.json(sections);
-        } else {
-            const articles = ((await Article.findAll({where: {published: true}})).map(article => article.get()));
-            const sections = ((await Section.findAll({order})).map(section => section.get()));
+        try {
+            if (req.user && req.user.signedIn) {
+                const sections = ((await Section.findAll({order})).map(section => section.get()));
+                res.json(sections);
+            } else {
+                const articles = ((await Article.findAll({where: {published: true}})).map(article => article.get()));
+                const sections = ((await Section.findAll({order})).map(section => section.get()));
 
-            const sectionAssociations = ((await ArticleSection.findAll()).map(association => association.get()));
-            let sectionsToShow = [];
-            sectionAssociations.map(association =>
-                articles.map(article => {
-                        if (article.id === association.ArticleId) {
-                            sections.map(section => {
-                                if (section.id === association.SectionId) {
-                                    sectionsToShow.push(section);
-                                }
-                            });
+                const sectionAssociations = ((await ArticleSection.findAll()).map(association => association.get()));
+                let sectionsToShow = [];
+                sectionAssociations.map(association =>
+                    articles.map(article => {
+                            if (article.id === association.ArticleId) {
+                                sections.map(section => {
+                                    if (section.id === association.SectionId) {
+                                        sectionsToShow.push(section);
+                                    }
+                                });
+                            }
                         }
-                    }
-                )
-            );
-            res.json(sectionsToShow.sort((a, b) => {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0); }));
+                    )
+                );
+                res.json(sectionsToShow.sort((a, b) => {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0); }));
+            }
+        } catch (e) {
+            res.status(403);
+            res.json({error: e});
         }
     });
 }
