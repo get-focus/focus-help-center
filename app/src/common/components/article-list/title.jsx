9695e9709b090e38b5ef13eb83879e36d58d0228
GuenoleK <kikabouguenole@gmail.com>
2016-07-28 19:25:14 +0200

    Updating the search render

--- app/src/common/components/article-list/title.jsx
+++ app/src/common/components/article-list/title.jsx
@@ -34,7 +34,7 @@ export default class ArticleListTitle extends React.Component {
 
     keyDownHandler = (e, filter) => {
         if (e.keyCode === 13) {
-            if (filter) {
+            if (!filter) {
                 this.props.loadArticleList();
             }
             this.props.router.push(`search?filter${filter ? `=${filter}` : ''}`);
@@ -43,7 +43,6 @@ export default class ArticleListTitle extends React.Component {
 
     render() {
         const {filter, search, error, articleList, isExtension} = this.props;
-        const {value} = this.state;
         return (
             <div className='article-list-header'>
                 <div className={'top-search'} style={isExtension ? {width: 330, background: '#FAFAFA', borderRadius: '2px', boxShadow: 'none', padding: '5px 3px', marginBottom: '8px'} : null}>
@@ -52,7 +51,7 @@ export default class ArticleListTitle extends React.Component {
                         <AutoComplete
                             errorText={error ? ' ' : null}
                             errorStyle={{color: 'indianred'}}
-                            searchText={value}
+                            searchText={filter}
                             hintText={i18n.t('search.placeholder') }
                             hintStyle={{color: isExtension ? '#BDBDBD' : 'white', bottom: 11, font: 'normal 16px Roboto,sans-serif'}}
                             inputStyle={{color: isExtension ? '#212121' : 'white'}}
@@ -74,7 +73,7 @@ export default class ArticleListTitle extends React.Component {
                                                 className='menu-item'
                                                 primaryText={article.title}
                                                 leftIcon={<i className='material-icons' style={{fontSize: 20, marginTop: 14.5, color: '#2196F3'}}>description</i>}
-                                                onClick={() => { this.props.router.push(`article/${article.id}`); this.setState({value: ''}); } }
+                                                onClick={() => this.props.router.push(`article/${article.id}`) }
                                                 />
                                         )
                                     };
