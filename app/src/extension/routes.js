9c20131f43b0cd4ccd6c923820af434341564a45
Damien FRIKHA <dfrikha@KLEE.LAN.NET>
2016-08-23 16:41:59 +0200

    Add redirect in extension

--- app/src/extension/routes.js
+++ app/src/extension/routes.js
@@ -1,5 +1,6 @@
 import {Home} from './views/home';
 import {Consult} from './views/consult';
+import {api} from '../common/server/api';
 
 export default {
     path: '/',
@@ -12,6 +13,18 @@ export default {
         {
             path: 'article/:id',
             component: Consult
+        },
+        {
+            path: 'query',
+            onEnter: async ({location: {query: {url, block}}}, redirect, done) => {
+                const article = await api.searchArticle(url, block);
+                if (typeof article === 'boolean') {
+                    redirect('home');
+                } else {
+                    redirect(`article/${article.id}`);
+                }
+                done();
+            }
         }
     ]
 };
